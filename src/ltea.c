469ca5d17988c66aa14e73f782353c5e807659a8
synodriver <diguohuangjiajinweijun@gmail.com>
2022-04-19 20:05:57 +0800

    坏源酱没处理字节序，太坏了QAQ

--- src/ltea.c
+++ src/ltea.c
@@ -5,8 +5,10 @@
 
 #ifdef _WIN32
 #define DLLEXPORT __declspec(dllexport)
+#define swap_uint32 _byteswap_ulong
 #elif
 #define DLLEXPORT
+#define swap_uint32 __builtin_bswap32
 #endif /* _WIN32 */
 
 
@@ -28,11 +30,20 @@ lencrypt_qq(lua_State *L)
     size_t key_len;
     const char *key = luaL_checklstring(L, 1, &key_len);
     luaL_argcheck(L, key_len == 16, 1, "key must be 16 bytes len!");
+#ifdef WORDS_BIGENDIAN
+    uint32_t *key_ = (uint32_t *)key;
+#else
+    uint32_t key_[4];
+    key_[0] = swap_uint32(((uint32_t *) key)[0]);
+    key_[1] = swap_uint32(((uint32_t *) key)[1]);
+    key_[2] = swap_uint32(((uint32_t *) key)[2]);
+    key_[3] = swap_uint32(((uint32_t *) key)[3]);
+#endif
     size_t src_len;
     const char *src = luaL_checklstring(L, 2, &src_len);
     int64_t out_len = encrypt_qq_len((int64_t) src_len);
     uint8_t *out = (uint8_t *) lua_newuserdata(L, (size_t) out_len);
-    int64_t buffer_updated = tea_encrypt_qq((uint32_t *) key, (uint8_t *) src, (int64_t) src_len, out, out_len);
+    int64_t buffer_updated = tea_encrypt_qq((uint32_t *) key_, (uint8_t *) src, (int64_t) src_len, out, out_len);
     if (buffer_updated < 0)
     {
         luaL_error(L, "encrypt wrong\n");
@@ -53,6 +64,15 @@ lencrypt(lua_State *L)
     size_t key_len;
     const char *key = luaL_checklstring(L, 1, &key_len);
     luaL_argcheck(L, key_len == 16, 1, "key must be 16 bytes len!");
+#ifdef WORDS_BIGENDIAN
+    uint32_t *key_ = (uint32_t *)key;
+#else
+    uint32_t key_[4];
+    key_[0] = swap_uint32(((uint32_t *) key)[0]);
+    key_[1] = swap_uint32(((uint32_t *) key)[1]);
+    key_[2] = swap_uint32(((uint32_t *) key)[2]);
+    key_[3] = swap_uint32(((uint32_t *) key)[3]);
+#endif
     size_t sumtable_len;
     const char *sumtable = luaL_checklstring(L, 2, &sumtable_len);
     luaL_argcheck(L, sumtable_len == 64, 2, "sum table must be 64 bytes len!");
@@ -61,7 +81,7 @@ lencrypt(lua_State *L)
     int64_t out_len = encrypt_qq_len((int64_t) src_len);
     uint8_t *out = (uint8_t *) lua_newuserdata(L, (size_t) out_len);
 
-    int64_t buffer_updated = tea_encrypt((uint32_t *) key, (uint32_t *) sumtable, (const uint8_t *) src,
+    int64_t buffer_updated = tea_encrypt((uint32_t *) key_, (uint32_t *) sumtable, (const uint8_t *) src,
                                          (int64_t) src_len, out, out_len);
     if (buffer_updated < 0)
     {
@@ -83,6 +103,15 @@ lencrypt_native_endian(lua_State *L)
     size_t key_len;
     const char *key = luaL_checklstring(L, 1, &key_len);
     luaL_argcheck(L, key_len == 16, 1, "key must be 16 bytes len!");
+#ifdef WORDS_BIGENDIAN
+    uint32_t *key_ = (uint32_t *)key;
+#else
+    uint32_t key_[4];
+    key_[0] = swap_uint32(((uint32_t *) key)[0]);
+    key_[1] = swap_uint32(((uint32_t *) key)[1]);
+    key_[2] = swap_uint32(((uint32_t *) key)[2]);
+    key_[3] = swap_uint32(((uint32_t *) key)[3]);
+#endif
     size_t sumtable_len;
     const char *sumtable = luaL_checklstring(L, 2, &sumtable_len);
     luaL_argcheck(L, sumtable_len == 64, 2, "sum table must be 64 bytes len!");
@@ -92,7 +121,7 @@ lencrypt_native_endian(lua_State *L)
     int64_t out_len = encrypt_qq_len((int64_t) src_len);
     uint8_t *out = (uint8_t *) lua_newuserdata(L, (size_t) out_len);
 
-    int64_t buffer_updated = tea_encrypt_native_endian((uint32_t *) key, (uint32_t *) sumtable, (const uint8_t *) src,
+    int64_t buffer_updated = tea_encrypt_native_endian((uint32_t *) key_, (uint32_t *) sumtable, (const uint8_t *) src,
                                                        (int64_t) src_len, out, out_len);
     if (buffer_updated < 0)
     {
@@ -114,11 +143,20 @@ ldecrypt_qq(lua_State *L)
     size_t key_len;
     const char *key = luaL_checklstring(L, 1, &key_len);
     luaL_argcheck(L, key_len == 16, 1, "key must be 16 bytes len!");
+#ifdef WORDS_BIGENDIAN
+    uint32_t *key_ = (uint32_t *)key;
+#else
+    uint32_t key_[4];
+    key_[0] = swap_uint32(((uint32_t *) key)[0]);
+    key_[1] = swap_uint32(((uint32_t *) key)[1]);
+    key_[2] = swap_uint32(((uint32_t *) key)[2]);
+    key_[3] = swap_uint32(((uint32_t *) key)[3]);
+#endif
     size_t src_len;
     const char *src = luaL_checklstring(L, 2, &src_len);
     uint8_t *out = (uint8_t *) lua_newuserdata(L, (size_t) src_len);
 
-    int64_t buffer_updated = tea_decrypt_qq((uint32_t *) key, (const uint8_t *) src, (int64_t) src_len, out,
+    int64_t buffer_updated = tea_decrypt_qq((uint32_t *) key_, (const uint8_t *) src, (int64_t) src_len, out,
                                             (int64_t) src_len);
     if (buffer_updated < 0)
     {
@@ -140,13 +178,22 @@ ldecrypt(lua_State *L)
     size_t key_len;
     const char *key = luaL_checklstring(L, 1, &key_len);
     luaL_argcheck(L, key_len == 16, 1, "key must be 16 bytes len!");
+#ifdef WORDS_BIGENDIAN
+    uint32_t *key_ = (uint32_t *)key;
+#else
+    uint32_t key_[4];
+    key_[0] = swap_uint32(((uint32_t *) key)[0]);
+    key_[1] = swap_uint32(((uint32_t *) key)[1]);
+    key_[2] = swap_uint32(((uint32_t *) key)[2]);
+    key_[3] = swap_uint32(((uint32_t *) key)[3]);
+#endif
     size_t sumtable_len;
     const char *sumtable = luaL_checklstring(L, 2, &sumtable_len);
     luaL_argcheck(L, sumtable_len == 64, 2, "sum table must be 64 bytes len!");
     size_t src_len;
     const char *src = luaL_checklstring(L, 3, &src_len);
     uint8_t *out = (uint8_t *) lua_newuserdata(L, (size_t) src_len);
-    int64_t buffer_updated = tea_decrypt((uint32_t *) key, (uint32_t *) sumtable, (const uint8_t *) src,
+    int64_t buffer_updated = tea_decrypt((uint32_t *) key_, (uint32_t *) sumtable, (const uint8_t *) src,
                                          (int64_t) src_len, out, (int64_t) src_len);
     if (buffer_updated < 0)
     {
@@ -168,13 +215,22 @@ ldecrypt_native_endian(lua_State *L)
     size_t key_len;
     const char *key = luaL_checklstring(L, 1, &key_len);
     luaL_argcheck(L, key_len == 16, 1, "key must be 16 bytes len!");
+#ifdef WORDS_BIGENDIAN
+    uint32_t *key_ = (uint32_t *)key;
+#else
+    uint32_t key_[4];
+    key_[0] = swap_uint32(((uint32_t *) key)[0]);
+    key_[1] = swap_uint32(((uint32_t *) key)[1]);
+    key_[2] = swap_uint32(((uint32_t *) key)[2]);
+    key_[3] = swap_uint32(((uint32_t *) key)[3]);
+#endif
     size_t sumtable_len;
     const char *sumtable = luaL_checklstring(L, 2, &sumtable_len);
     luaL_argcheck(L, sumtable_len == 64, 2, "sum table must be 64 bytes len!");
     size_t src_len;
     const char *src = luaL_checklstring(L, 3, &src_len);
     uint8_t *out = (uint8_t *) lua_newuserdata(L, (size_t) src_len);
-    int64_t buffer_updated = tea_decrypt_native_endian((uint32_t *) key, (uint32_t *) sumtable, (const uint8_t *) src,
+    int64_t buffer_updated = tea_decrypt_native_endian((uint32_t *) key_, (uint32_t *) sumtable, (const uint8_t *) src,
                                                        (int64_t) src_len, out, (int64_t) src_len);
     if (buffer_updated < 0)
     {
